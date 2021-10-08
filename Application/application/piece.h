7b5a98a440117fac91236a8b68c4453c51b1283f
Ben Ralston <ben.ralston12@gmail.com>
2021-10-07 22:46:36 -0400

    Delete uppercase files and directories

--- Application/application/piece.h
+++
@@ -1,7 +0,0 @@
-#ifndef PIECE_H
-#define PIECE_H
-
-enum Piece { None = -1, WhitePawn, WhiteKnight, WhiteBishop, WhiteRook, WhiteQueen, WhiteKing,
-             BlackPawn, BlackKnight, BlackBishop, BlackRook, BlackQueen, BlackKing };
-
-#endif // PIECE_H
